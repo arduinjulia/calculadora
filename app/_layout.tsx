67d3c633a47de71040ffa71bd29f365af8cc09ab
Your Name <juliaarduin@hotmail.com>
2026-03-04 21:24:04 -0300

    Calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,74 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [conta, setConta] = useState("")
+  const [resultado, setResultado] = useState("")
+
+  function adicionarNumero(numero: string) {
+    setConta((valorAnterior) => valorAnterior + numero);
+  }
+
+
+  function calcular() {
+    const resultado = eval(conta);
+    setResultado(resultado.toString());
+  }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <SafeAreaView style={styles.fundo} >
+      <Text style={styles.conta}>{conta}</Text>
+      <Text style={styles.resultado}>{resultado}</Text> 
+
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("1")}>1</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("2")}>2</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("3")}>3</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("4")}>4</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("5")}>5</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("6")}>6</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("7")}>7</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("8")}>8</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("9")}>9</TouchableOpacity>  
+      <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("0")}>0</TouchableOpacity> 
+      <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("+")}>+</TouchableOpacity> 
+      <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("-")}>-</TouchableOpacity> 
+      <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("*")}>x</TouchableOpacity>
+      <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("/")}>/</TouchableOpacity>   
+      <TouchableOpacity style={styles.botaoSimbolos} onPress={calcular}>=</TouchableOpacity>   
+    </SafeAreaView>
+
   );
 }
+
+const styles = StyleSheet.create({
+  fundo:{
+    backgroundColor: 'black'
+  },
+
+  resultado:{
+    color:'#ffffff'
+  },
+
+  conta:{
+    color: '#ffffff'
+  },
+
+  botaoNumero:{
+		backgroundColor: '#303b33',
+		marginHorizontal: 10,
+		padding: 20,
+		borderRadius: 30,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+
+  botaoSimbolos:{
+		backgroundColor: '#ff7605',
+		marginHorizontal: 10,
+		padding: 20,
+		borderRadius: 30,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+})
