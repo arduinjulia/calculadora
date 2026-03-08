6b3fa18be3b7ca6b2d1b87a61217ff1a789d4e54
Your Name <juliaarduin@hotmail.com>
2026-03-08 20:29:36 -0300

    continuação calculadora

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,42 +16,57 @@ export default function RootLayout() {
     setResultado(resultado.toString());
   }
 
+  function apagarDigito(){
+    setConta((valorAnterior) => valorAnterior.slice(0, -1))
+  }
+
+  function limparConta(){
+    setConta("")
+    setResultado("")
+  }
+
   return (
     <SafeAreaView style={styles.fundo} >
-      <Text style={styles.conta}>{conta}</Text>
-      <Text style={styles.resultado}>{resultado}</Text> 
-
-      <View style={styles.linha}>
-        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("V")}>x</TouchableOpacity>       
+      <View style={styles.display}>
+        <Text style={styles.conta}>{conta}</Text>
+        <Text style={styles.resultado}>{resultado}</Text>
       </View>
 
       <View style={styles.linha}>
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("7")}>7</TouchableOpacity>  
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("8")}>8</TouchableOpacity>  
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("9")}>9</TouchableOpacity> 
-        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("*")}>x</TouchableOpacity>       
+        <TouchableOpacity style={styles.botaoNumero} ></TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => limparConta()}>AC</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => apagarDigito()}>C</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("/")}>/</TouchableOpacity>
       </View>
 
       <View style={styles.linha}>
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("4")}>4</TouchableOpacity> 
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("5")}>5</TouchableOpacity>  
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("7")}>7</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("8")}>8</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("9")}>9</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("*")}>x</TouchableOpacity>
+      </View>
+
+      <View style={styles.linha}>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("4")}>4</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("5")}>5</TouchableOpacity>
         <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("6")}>6</TouchableOpacity>
         <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("-")}>-</TouchableOpacity>
       </View>
 
       <View style={styles.linha}>
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("1")}>1</TouchableOpacity>  
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("2")}>2</TouchableOpacity>  
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("3")}>3</TouchableOpacity>  
-        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("+")}>+</TouchableOpacity> 
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("1")}>1</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("2")}>2</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("3")}>3</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("+")}>+</TouchableOpacity>
       </View>
 
 
 
       <View style={styles.linha}>
-        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("0")}>0</TouchableOpacity> 
-        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("/")}>/</TouchableOpacity>   
-        <TouchableOpacity style={styles.botaoSimbolos} onPress={calcular}>=</TouchableOpacity>   
+        <TouchableOpacity style={styles.botaoNumero}></TouchableOpacity>
+        <TouchableOpacity style={styles.botaoNumero} onPress={() => adicionarNumero("0")}>0</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoSimbolos} onPress={calcular}>=</TouchableOpacity>
+        <TouchableOpacity style={styles.botaoSimbolos} onPress={() => adicionarNumero("V")}>v</TouchableOpacity>
       </View>
 
     </SafeAreaView>
@@ -59,45 +74,53 @@ export default function RootLayout() {
 }
 
 const styles = StyleSheet.create({
-  fundo:{
+  fundo: {
     backgroundColor: 'black'
   },
 
-  resultado:{
-    color:'#ffffff'
+  resultado: {
+    color: '#ffffff',
+    fontSize: 70
   },
 
-  conta:{
+  conta: {
+    color: '#ffffff',
+    fontSize: 70,
+  },
+
+  botaoNumero: {
+    backgroundColor: '#303b33',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginHorizontal: 10,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 50,
     color: '#ffffff'
   },
 
-  botaoNumero:{
-		backgroundColor: '#303b33',
+  botaoSimbolos: {
+    backgroundColor: '#ff7605',
     width: 100,
     height: 100,
     borderRadius: 50,
-		marginHorizontal: 10,
-		padding: 20,
-		justifyContent: 'center',
-		alignItems: 'center',
+    marginHorizontal: 10,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
     fontSize: 50,
-    color:'#ffffff'
-	},
+    color: '#ffffff'
+  },
 
-  botaoSimbolos:{
-		backgroundColor: '#ff7605',
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-		marginHorizontal: 10,
-		padding: 20,
-		justifyContent: 'center',
-		alignItems: 'center',
-    fontSize: 50,
-    color:'#ffffff'
-	},
-   
-  linha:{
+  linha: {
     flexDirection: "row"
+  },
+
+  display:{
+    height: 200,
+    justifyContent: "flex-end",
+    padding: 20
   }
 })
